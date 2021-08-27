a4d5bce7287f2be55d23735f509d1fc54ddf29cd
achur00-github <okunadeyusuf003@gmail.com>
2021-08-27 19:38:29 +0100

    delete

--- Website_Project/storage/framework/views/141e7d5c428cc766cc24210e6f782a1aea1c8383.php
+++
@@ -1,6 +0,0 @@
-
-<?php $__env->startSection('content'); ?>
-    <h1><?php echo e($title); ?></h1>
-    <p>become the best you can be</p>
-<?php $__env->stopSection(); ?>     
-<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\staunchwebsitePJ\resources\views/Pages/index.blade.php ENDPATH**/ ?>
